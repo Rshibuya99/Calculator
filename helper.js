a2bc397b9dfc7297c86ac2af87e389fd4e9056f9
Rocco Shibuya <shibuyaryouken@gmail.com>
2024-06-16 18:52:52 +0800

    create helper.js with basic operator functions

---
+++ helper.js
@@ -0,0 +1,36 @@
+function operateAdd(x, y) {
+    return x + y
+}
+
+function operateSubtract(x, y) {
+    return x - y
+}
+
+function operateMultiply(x, y) {
+    return x * y
+}
+
+function operateDivide(x, y) {
+    return x / y
+}
+
+function operate(x, y, operator) {
+    if (operator == "+") {
+        return operateAdd(x, y)
+    }
+    else if (operator == "-") {
+        return operateSubtract(x, y)
+    }
+    else if (operator == "*") {
+        return operateMultiply(x, y)
+    }
+    else if (operator == "/") {
+        if (y == 0) {
+            throw new RangeError("Cannot divide by zero")
+        }
+        return operateDivide(x, y)
+    }
+    else {
+        return NaN
+    }
+}
