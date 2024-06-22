354637ce9b9a4ec590400a882a4dc2cce1bae183
Rocco Shibuya <shibuyaryouken@gmail.com>
2024-06-22 23:05:59 +0800

    attach clear function and listed main TODOs

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 /*
-
+1. display
+2. store
+    num 1
+        after press numeric button
+        after display calulation output / press equal sign
+    num 2
+        after press operators
+    operator
+        after press operators
+3. calculate
+4. display
 
 
 */
@@ -8,8 +18,42 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
+    // variables
     let numX = NaN;
     let numY = NaN;
+    let operator = NaN;
+
+    const DISPLAY = document.querySelector('.display');
+    const MAIN_CONTAINER = document.querySelector('.container-main');
+    const calculate = document.querySelector('#calculate');
+
+    MAIN_CONTAINER.addEventListener('click', function(e) {
+        
+        // display numbers
+        if (e.target.classList.contains("numeric")) {
+            if (DISPLAY.textContent == '0') {
+                DISPLAY.textContent = `${e.target.textContent}`;
+            }
+            else {
+                DISPLAY.textContent = `${DISPLAY.textContent}${e.target.textContent}`;
+            }
+            storeNum();
+        }
+
+        // reset display and variables
+        if (e.target.classList.contains("clear")) {
+            numX                = NaN;
+            numY                = NaN;
+            operator            = NaN;
+            DISPLAY.textContent = '0';
+        }
+
+    })
+
+    // calculate
+    calculate.addEventListener('click', function() {
+        alert(`${DISPLAY.textContent}`);
+    })
 
 
 })
